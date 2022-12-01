1b8a58250ffe84c11c8fb20cd9e74432b898974b
Ådne Ovesen <adne.wolden.ovesen@wideroe.no>
2022-12-01 13:07:29 +0100

    Add project files.

---
+++ AdventOfCode2022/Dec1/CalorieCounting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Dec1
+{
+    public class CalorieCounting
+    {
+        public static int GetCaloriesFromElf(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            
+            var calorie = 0;
+            var calories = new List<int>();
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    calories.Add(calorie);
+                    calorie = 0;
+                    continue;
+                }
+                calorie += int.Parse(line);
+            }
+            calories.Add(calorie);
+
+            calories.Sort((a, b) => b.CompareTo(a));
+
+            return calories[0];
+        }
+
+        public static int GetCaloriesFromTopThreeElves(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            
+            var calorie = 0;
+            var calories = new List<int>();
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    calories.Add(calorie);
+                    calorie = 0;
+                    continue;
+                }
+                calorie += int.Parse(line);
+            }
+            calories.Add(calorie);
+
+            calories.Sort((a, b) => b.CompareTo(a));
+            
+            return calories[0] + calories[1] + calories[2];
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2022.Dec1;
+
+var path = "..\\..\\..\\Dec1\\input1.txt";
+Console.WriteLine($"Elf with the most calories has: {CalorieCounting.GetCaloriesFromElf(path)}");
+
+Console.WriteLine($"The top three elves has: {CalorieCounting.GetCaloriesFromTopThreeElves(path)}");
