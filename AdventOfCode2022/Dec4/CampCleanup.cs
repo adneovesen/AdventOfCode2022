7fa05acbdc4c2fb9cf1fd9e0dcffc07baf342829
Ådne Ovesen <adnewo@hotmail.com>
2022-12-04 11:40:36 +0100

    Forloop in forloop in foreachloop 🤡

---
+++ AdventOfCode2022/Dec4/CampCleanup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Dec4
+{
+    public class CampCleanup
+    {
+        public static int RangeFullyContained(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            var score = 0;
+
+            foreach(var line in lines)
+            {
+                var sections = line.Split(",");
+                var firstSection = sections[0].Split("-");
+                var secondSection = sections[1].Split("-");
+
+                if (int.Parse(firstSection[0]) == int.Parse(secondSection[0]) || int.Parse(firstSection[1]) == int.Parse(secondSection[1]))
+                {
+                    score++;
+                    continue;
+                }
+                if(int.Parse(firstSection[0]) < int.Parse(secondSection[0]))
+                {
+                    if(int.Parse(firstSection[1]) > int.Parse(secondSection[1]))
+                    {
+                        score++;
+                        continue;
+                    }
+                }
+                if (int.Parse(firstSection[0]) > int.Parse(secondSection[0]))
+                {
+                    if (int.Parse(firstSection[1]) < int.Parse(secondSection[1]))
+                    {
+                        score++;
+                        continue;
+                    }
+                }
+            }
+
+            return score;
+        }
+
+        public static int RangeOverlap(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            var score = 0;
+
+            foreach (var line in lines)
+            {
+                var sections = line.Split(",");
+
+                var firstSection = sections[0].Split("-").Select(int.Parse).ToArray();
+                var secondSection = sections[1].Split("-").Select(int.Parse).ToArray();
+
+                var jankestBooleanValue = false;
+
+                for(int i = firstSection[0]; i <= firstSection[1]; i++)
+                {
+                    for (int j = secondSection[0]; j <= secondSection[1]; j++)
+                    {
+                        if(i == j)
+                        {
+                            score++;
+                            jankestBooleanValue = true;
+                            break;
+                        }
+                    }
+                    if (jankestBooleanValue) break;
+                }
+            }
+
+            return score;
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -2,14 +2,24 @@
 using AdventOfCode2022.Dec1;
 using AdventOfCode2022.Dec2;
 using AdventOfCode2022.Dec3;
+using AdventOfCode2022.Dec4;
 
-//var path = "..\\..\\..\\Dec2\\input.txt";
-//Console.WriteLine($"Total score if everything goes like the strategy guide : {RockPaperScissors.TotalScoreFromTournament(path)}");
-//
-//Console.WriteLine($"Total score if everything goes like the actual strategy guide : {RockPaperScissors.TotalScoreFromTournament(path, true)}");
+/*
+var path = "..\\..\\..\\Dec2\\input.txt";
+Console.WriteLine($"Total score if everything goes like the strategy guide : {RockPaperScissors.TotalScoreFromTournament(path)}");
+
+Console.WriteLine($"Total score if everything goes like the actual strategy guide : {RockPaperScissors.TotalScoreFromTournament(path, true)}");
 
 var path = "..\\..\\..\\Dec3\\input.txt";
 
 Console.WriteLine($"Sum of the priorities of item types in part one: {RucksackReorganization.SumOfItemsInBothCompartments(path)}");
 
 Console.WriteLine($"Sum of the priorities of item types in part two: {RucksackReorganization.SumOfBadgePriorities(path)}");
+
+*/
+
+var path = "..\\..\\..\\Dec4\\input.txt";
+
+Console.WriteLine($"In how many assignment pairs does one range fully contain the other? Answer : {CampCleanup.RangeFullyContained(path)}");
+
+Console.WriteLine($"In how many assignment pairs do the ranges overlap? Answer: {CampCleanup.RangeOverlap(path)}");
